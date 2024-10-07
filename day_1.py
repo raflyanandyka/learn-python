4580140f945c599b1599bc2a7fdb384f9981f26d
raflyanandyka <rafly.anandykaa@gmail.com>
2024-10-07 20:07:38 +0700

    added day 1 project

---
+++ day_1.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator!")
+
+city = input("Enter the name of city: ")
+pet = input("Enter the name of your pet: ")
+
+print("Your band name could be: " + city + " " + pet)
